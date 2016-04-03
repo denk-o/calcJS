1cbc97230fe84ffc65c60611eefd3f88e64cdc2e
Kevin Liu <kevinliu314@gmail.com>
2016-04-03 18:13:20 -0400

    additional button functions implemented

--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
+function getOutput(){
+  return $(".output").text();
+}
+
+
+
 $(document).ready(function(){
   console.log("test0123456789");
   $(".number").click(function(){
-    alert($(this).children('.item').text());
-    console.log($(this).children('.item').text());
     $(".output").append($(this).children('.item').text());
+  });
+  $(".operation").click(function(){
+      $(".output").append($(this).children('.item').text());
+  });
+  $(".equals").click(function(){
+    var calculation = getOutput();
+    console.log(eval(calculation));
+  });
+  $(".clear").click(function(){
+    if($(this).children('.item').text()=="AC"){
+      //clear memory
+      alert("ac");
+      $(".output").html('');
+    }
+    if($(this).children('.item').text()=="CE"){
+      //clear entry
+      alert("ce");
+      $(".output").html('');
+    }
   });
 });
