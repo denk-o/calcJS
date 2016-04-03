6bd5d6e425bba1950734bf4bf2b88cd298626c81
Kevin Liu <kevinliu314@gmail.com>
2016-04-03 13:57:17 -0400

    fixed annoying bug issue

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  console.log("test");
+  console.log("test0123456789");
   $(".number").click(function(){
+    alert($(this).children('.item').text());
     console.log($(this).children('.item').text());
     $(".output").append($(this).children('.item').text());
   });
